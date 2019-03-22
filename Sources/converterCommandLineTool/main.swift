94df3f4b5b8ace1483e98644fd3514068a5fc97e
Mariana Soriano <mariana.soriano.licona@gmail.com>
2019-03-22 14:46:54 -0600

    Solve code execution according to user's choices :)

--- Sources/converterCommandLineTool/main.swift
+++ Sources/converterCommandLineTool/main.swift
@@ -18,26 +18,32 @@ extension StringProtocol { //A protocol defines a blueprint of methods, properti
 }
 
 func convertCentimetersToInches() {
-    print("First How many centimeters do you want to convert to inches? Press 'Q' to Quit")
+    print("Press 'S' to Start or Press 'Q' to Quit")
     let str: String = readLine()!
-    repeat {
-        print("Second How many centimeters do you want to convert to inches? Press 'Q' to Quit")
-        let str: String = readLine()!
-        if str == "Q" || str == "q" {
-            print("Bye!")
-            break
-        }
-        else if str != "Q" || str != "q" {
-            if let value = str.double  {
-                let fromCmToIn = value / centimeter
-                print("\(value) centimeters are \(Float(fromCmToIn)) inches")
+    if str == "Q" || str == "q" {
+        print("Come back soon!")
+    }
+    else if str == "S" || str == "s" {
+        while str != "Q" {
+            print("\nHow many centimeters do you want to convert to inches? Press 'Q' to Quit") //\n line break
+            let str: String = readLine()!
+            if str == "Q" || str == "q" {
+                print("Bye!")
+                break
             }
-            else { 
-                print("Nested else. Invalid Input. Please enter only numbers")
-            } 
-        }
-    }  while str != nil 
+            else if str != "Q" || str != "q" {
+                if let value = str.double  {
+                    let fromCmToIn = value / centimeter
+                    print("\(value) centimeters are \(Float(fromCmToIn)) inches")
+                }
+                else { 
+                    print("Invalid Input. Please enter only numbers")
+                } 
+            }
+        } 
+    }
 }
+// TODO: Finish "Inches" code.
 func convertInchesToCentimeters() {
     print("How many inches do you want to convert to centimeters?")
     let str: String = readLine()!
@@ -52,12 +58,15 @@ func convertInchesToCentimeters() {
         print("Invalid Input. Please enter only numbers")
     }
 }
+// TODO: Do "Ounces" code.
+// TODO: Do "Grams" code.
 
+// TODO: Complete 'O' Onces and 'G' Grams options
 print("Welcome. Press 'C' key if you want to convert Centimeters to inches and 'I' key if you want to convert Inches to centimeters.")
 let usersChoice: String = readLine()!
 
 if  usersChoice == "C" || usersChoice == "c" { // || "or" logical operator 
-        convertCentimetersToInches()  // Convert Centimeters to Inches Function
+    convertCentimetersToInches()  // Convert Centimeters to Inches Function
 }
 else if usersChoice == "I" || usersChoice == "i" {
     convertInchesToCentimeters()  // Convert Inches to Centimeters Function
@@ -65,11 +74,9 @@ else if usersChoice == "I" || usersChoice == "i" {
     print("Invalid Key. Please press 'C' or 'I' keys")
 }
 
-
-
-// TODO: Solve "cannot convert value of type 'String' to expected argument type 'Bool' "  issue
-// TODO: Ask the user their values and do calculation ✔︎ 
-// TODO: Convert "inputIn" var into an Integer and use it in fromInToCm var ✔︎ 
-// TODO: Solve "incorrect" user's input issue. What happens if the user doesn't give a number as input? ✔︎ 
-// TODO: Use "let value" to do math and print result ✔︎ 
-// TODO: Ask user if they want to convert inches or centimeters. Create a Conditional Statement to execute corresponding code ✔︎ 
+// DONE: Solve "cannot convert value of type 'String' to expected argument type 'Bool' "  issue
+// DONE: Ask the user their values and do calculation
+// DONE: Convert "inputIn" var into an Integer and use it in fromInToCm var
+// DONE: Solve "incorrect" user's input issue. What happens if the user doesn't give a number as input?
+// DONE: Use "let value" to do math and print result
+// DONE: Ask user if they want to convert inches or centimeters. Create a Conditional Statement to execute corresponding code
