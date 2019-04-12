5e03ce3349626373c6c629ff3416d6fd57639110
Mariana Soriano <mariana.soriano.licona@gmail.com>
2019-04-12 13:03:17 -0500

    Trying to apply 'Single responsibility' principle

--- Sources/converterCommandLineTool/main.swift
+++ Sources/converterCommandLineTool/main.swift
@@ -1,3 +1,17 @@
+// function to catch User Input
+func catchUserInput(unitStr: String, equivalentUnitStr: String) -> String {
+    print("\nHow many \(unitStr) do you want to convert to \(equivalentUnitStr)? Press 'Q' to Quit")
+    let str: String = readLine()!
+    return str
+}
+var userInput = catchUserInput(unitStr: "centimeters", equivalentUnitStr: "inches")
+print("User input was \(userInput)")
+
+// function to validate User Input
+func validateUserInput() {
+
+}
+
 //Convert User’s Value (centimeters or grams) into Equivalent Unit (inches or oz)/
 func division(userValue: Double, unit: Double) -> Double {
     let result = userValue / unit
@@ -11,5 +25,6 @@ Press 'C' key to convert Centimeters to Inches
 """)
 let usersChoice: String = readLine()!
 if  usersChoice == "C" || usersChoice == "c" {
+    catchUserInput(unitStr: "centimeters", equivalentUnitStr: "inches")
     division(userValue: 12.7, unit: 2.54)
 }
